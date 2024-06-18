0ccf15d18b18732fc9ff50f551ca662d3b1a8d05
RmNLcN06 <romain.luciano@orange.fr>
2024-06-18 12:21:03 +0200

    Add GetContinent() (GET) method

---
+++ Controllers/ContinentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WrestleApplicationAPI.Datas;
+using WrestleApplicationAPI.Entities;
+
+namespace WrestleApplicationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContinentsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContinentsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Continent>>> GetContinents()
+        {
+            return await _context.Continents.ToListAsync();
+        }
+    }
+}
