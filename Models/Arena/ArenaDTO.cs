81fce858f4c746b3161a76c1f232f1fb348ba95d
RmNLcN06 <romain.luciano@orange.fr>
2024-07-04 11:32:44 +0200

    Add Arena DTO Model

---
+++ Models/Arena/ArenaDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WrestleApplicationAPI.Models.Arena
+{
+    public class ArenaDTO
+    {
+        public int IdArena { get; set; }
+
+        public string NameArena { get; set; } = string.Empty;
+    }
+}
